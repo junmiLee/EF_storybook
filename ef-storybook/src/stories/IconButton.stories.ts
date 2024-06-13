cb26875f67e18580e88394f4a85680be8d2b4d3d
junmiLee <junmile@ef-ems.com>
2024-06-13 16:59:44 +0900

    📁 : icon 이미지 추가

---
+++ ef-storybook/src/stories/IconButton.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import IconButton from "./components/IconButton";
+import { fn } from "@storybook/test";
+
+const meta = {
+  title: "Buttons/IconButton",
+  component: IconButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    alt: { control: "text", description: "이미지의 alt 속성" },
+    iconPath: { control: "text", description: "이미지 경로" },
+    onClick: { action: "clicked", description: "버튼 클릭 이벤트" },
+  },
+  args: {
+    onClick: fn(),
+  },
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    alt: "icon",
+    iconPath: "/icons/ic-delete-dark.svg",
+  },
+};
