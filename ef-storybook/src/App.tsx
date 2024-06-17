8caa741656face657ca97c1ea7abbc6250a1d16c
junmiLee <junmile@ef-ems.com>
2024-06-17 17:52:17 +0900

    📚 섹션1-9 Generic Type과 Event Bubbling을 활용한 TagList 컴포넌트

--- ef-storybook/src/App.tsx
+++ ef-storybook/src/App.tsx
@@ -3,41 +3,48 @@ import logo from "./logo.svg";
 import "./App.css";
 import Label from "./stories/components/Label";
 import DefaultTextField from "./stories/components/DefaultTextField";
+import TagList from "./stories/components/TagList";
 
 function App() {
   const [isError, setIsError] = useState(false);
   return (
-    <div className=" text-mono200 w-[400px] p-20">
-      <Label htmlFor="email">이메일</Label>
-      <DefaultTextField
-        id="email"
-        placeholder="이메일을 입력하세요"
-        value=""
-        errorMessage="이메일을 확인해주세요"
-        isError={isError}
-        iconAlt="delete"
-        iconPath="/icons/ic-delete-dark.svg"
-        onChange={() => {}}
-        onIconClick={() => {}}
-      />
-
-      <div className=" my-6">
-        <Label htmlFor="address">주소</Label>
-        <DefaultTextField
-          id="address"
-          placeholder="주소을 입력하세요"
-          value=""
-          errorMessage="주소을 확인해주세요"
-          isError={isError}
-          iconAlt="delete"
-          iconPath="/icons/ic-delete-dark.svg"
-          onChange={() => {}}
-          onIconClick={() => {}}
-        />
-      </div>
-
-      <button onClick={() => setIsError((prev) => !prev)}>에러 토글</button>
+    <div className="bg-primary w-screen h-screen">
+      <TagList
+        tagList={["All", "React", "TypeScript"]}
+        onTagClick={console.log}
+      ></TagList>
     </div>
+
+    // <div className=" text-mono200 w-[400px] p-20">
+    //   <Label htmlFor="email">이메일</Label>
+    //   <DefaultTextField
+    //     id="email"
+    //     placeholder="이메일을 입력하세요"
+    //     value=""
+    //     errorMessage="이메일을 확인해주세요"
+    //     isError={isError}
+    //     iconAlt="delete"
+    //     iconPath="/icons/ic-delete-dark.svg"
+    //     onChange={() => {}}
+    //     onIconClick={() => {}}
+    //   />
+
+    //   <div className=" my-6">
+    //     <Label htmlFor="address">주소</Label>
+    //     <DefaultTextField
+    //       id="address"
+    //       placeholder="주소을 입력하세요"
+    //       value=""
+    //       errorMessage="주소을 확인해주세요"
+    //       isError={isError}
+    //       iconAlt="delete"
+    //       iconPath="/icons/ic-delete-dark.svg"
+    //       onChange={() => {}}
+    //       onIconClick={() => {}}
+    //     />
+    //   </div>
+    //   <button onClick={() => setIsError((prev) => !prev)}>에러 토글</button>
+    // </div>
   );
 }
 
